7751890d23cb48a9d0545a0b90fb72fa8f76ca39
amakso <george.amakso@gmail.com>
2024-04-12 12:42:24 +0300

    sdasd

--- final.sql
+++ final.sql
@@ -374,7 +374,7 @@ END //
 DELIMITER ;
  
 DELIMITER //
- 
+
 CREATE PROCEDURE manage_application(
     IN employee_username VARCHAR(30),
     IN job_id INT(11),
@@ -384,19 +384,19 @@ BEGIN
     DECLARE evaluator1_username VARCHAR(30);
     DECLARE evaluator2_username VARCHAR(30);
     DECLARE application_exists INT;
- 
+
     SELECT evaluator INTO evaluator1_username
     FROM job
     WHERE id = job_id;
     
-	SELECT evaluator INTO evaluator2_username
+     SELECT evaluator INTO evaluator2_username
     FROM job
     WHERE id = job_id;
- 
+
     SELECT COUNT(*) INTO application_exists
     FROM applies
     WHERE cand_usrname = employee_username AND job_id = job_id;
- 
+
     IF action = 'i' THEN
         IF application_exists = 0 THEN
             INSERT INTO applies (cand_usrname, job_id)
@@ -405,36 +405,36 @@ BEGIN
             IF evaluator1_username = 'unknown' THEN
                 UPDATE job SET evaluator = (SELECT username FROM evaluator WHERE firm = (SELECT firm FROM employee WHERE username = employee_username)) WHERE id = job_id;
             END IF;
- 
+
             IF evaluator2_username = 'unknown' THEN
                 UPDATE job SET evaluator1 = (SELECT username FROM evaluator WHERE firm = (SELECT firm FROM employee WHERE username = employee_username)) WHERE id = job_id;
             END IF;
- 
+
             SELECT 'successful creation.' AS message;
         ELSE
             SELECT 'application exists.' AS message;
         END IF;
- 
+
     ELSEIF action = 'c' THEN
         IF application_exists > 0 THEN
-            DELETE FROM applies WHERE cand_usrname = employee_username AND job_id = job_id;
+            UPDATE applies SET status = 'inactive' WHERE cand_usrname = employee_username AND job_id = job_id;
             SELECT 'application canceled' AS message;
         ELSE
             SELECT 'no application to cancel' AS message;
         END IF;
- 
+
     ELSEIF action = 'a' THEN
         IF application_exists > 0 THEN
-            SELECT 'application activated successfully.' AS message;
+            UPDATE applies SET status = 'active' WHERE cand_usrname = employee_username AND job_id = job_id;
         ELSE
             SELECT 'no canceled application to activate.' AS message;
         END IF;
     ELSE
         SELECT 'dwse a,c or a' AS message;
     END IF;
- 
+
 END //
- 
+
 DELIMITER ;
 
 DELIMITER //
